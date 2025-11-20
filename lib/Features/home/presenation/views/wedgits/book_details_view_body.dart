ba3df29f1c552b158ec95004d635f32cafc30bc5
mohamed hassan <63217614+Moamed1010@users.noreply.github.com>
2025-11-20 03:23:36 +0200

    add title and subtitle to details

--- lib/Features/home/presenation/views/wedgits/book_details_view_body.dart
+++ lib/Features/home/presenation/views/wedgits/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Core/utils/Styles.dart';
 import 'package:bookly/Features/home/presenation/views/wedgits/Custom_book_details_appbar.dart';
 import 'package:bookly/Features/home/presenation/views/wedgits/Custom_book_Item.dart';
 import 'package:flutter/material.dart';
@@ -7,21 +8,32 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width=MediaQuery.of(context).size.width;
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
-        
-        const CustomBookDetailsAppBar(),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width*.17),
-          child: const CustomBookImage(),
-        ),
-      
+          const CustomBookDetailsAppBar(),
+           SizedBox(height: 20,),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .23),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(height: 43),
+          Text(
+            "The Junle Book",
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 6),
+          Text(
+            "Rudyaed Kipling",
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
