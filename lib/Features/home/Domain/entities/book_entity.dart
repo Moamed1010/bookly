643a3ae1288813a92dedeec3e7cbf2dc758fcf6b
mohamed hassan <63217614+Moamed1010@users.noreply.github.com>
2025-11-23 15:59:53 +0200

    add book entitiy

---
+++ lib/Features/home/Domain/entities/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String Name;
+  final String Title;
+  final String AutherName;
+  final num Price;
+  final num Rating;
+
+  BookEntity({
+    required this.Name,
+    required this.Title,
+    required this.AutherName,
+    required this.Price,
+    required this.Rating,
+  });
+}
