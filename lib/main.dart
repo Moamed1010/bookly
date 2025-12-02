7f279bfccbfe8b293eab27782613faeefd97f296
mohamed hassan <63217614+Moamed1010@users.noreply.github.com>
2025-12-02 17:50:46 +0200

    register adapter and oben featued box

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:bookly/Core/utils/app_router.dart';
+import 'package:bookly/Features/home/Domain/entities/book_entity.dart';
 import 'package:bookly/constans.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main() async{
   runApp(const BooklyApp());
+  Hive.registerAdapter(BookEntityAdapter());
+ await Hive.openBox(KFeaturedBox);
 }
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
