d5bfed79126772a1414e18f02aea462d4c552e7e
mohamed hassan <63217614+Moamed1010@users.noreply.github.com>
2025-12-02 17:55:25 +0200

    catch featured box

---
+++ lib/Core/utils/functions/save_box.dart
@@ -0,0 +1,8 @@
+  import 'package:bookly/Features/home/Domain/entities/book_entity.dart';
+import 'package:hive/hive.dart';
+
+void saveBoxData(List<BookEntity> books,String boxName) {
+    var box=Hive.box(boxName);
+    box.addAll(books);
+  }
+  
--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,6 +1,9 @@
 import 'package:bookly/Core/utils/api_service.dart';
+import 'package:bookly/Core/utils/functions/save_box.dart';
 import 'package:bookly/Features/home/Domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/data/Models/book_model/book_model.dart';
+import 'package:bookly/constans.dart';
+
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeatureBooks();
@@ -27,9 +30,11 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
       endpoint: 'volumes?Filtering=free-ebooks&sorting=newest&q=programming',
     );
     List<BookEntity> books = get_books_list(data);
+    saveBoxData(books,KFeaturedBox);
     return books;
   }
-  
+
+
   List<BookEntity> get_books_list(Map<String, dynamic> data) {
     List<BookEntity> books = [];
     for (var bookmap in data['items']) {
